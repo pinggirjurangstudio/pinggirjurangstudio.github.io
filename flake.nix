80a272775427f406782634c9144b147e831ab1a9
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2025-10-03 17:38:43 +0700

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "pinggirjurang.studio";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            name = "pinggirjurang.studio";
+            buildInputs = with pkgs; [
+              hugo
+            ];
+          };
+        }
+      );
+    };
+}
